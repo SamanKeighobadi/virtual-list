790e12a4eaac457d7cba6bb6c92360d6d49efc50
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-07-03 20:00:27 +0430

    create post card component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
 import React from "react";
-import { List, AutoSizer } from "react-virtualized";
+import {
+  List,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+} from "react-virtualized";
 
 import useSWR from "swr";
+import PostCard from "./components/PostCard";
 
 const App = () => {
   const { data: posts } = useSWR(`https://jsonplaceholder.typicode.com/posts`);
+  const cash = React.useRef(
+    new CellMeasurerCache({
+      fixedWidth: true,
+      defaultHeight: 100,
+    })
+  );
 
   return (
-    <div className="px-10 py-6 bg-slate-400 min-h-screen">
-      <header className="App-header"></header>
-      <h1 className="text-xl text-slate-500">saman</h1>
+    <div className=" bg-slate-400 min-h-screen">
+      <div style={{ width: "100%", height: "100vh" }}>
+        <AutoSizer>
+          {({ height, width }) => (
+            <List
+              height={height}
+              width={width}
+              rowCount={posts.length}
+              rowHeight={cash.current.rowHeight}
+              deferredMeasurementCache={cash.current}
+              rowRenderer={({ index, key, style, parent }) => {
+                const post = posts[index];
 
-      <List
-        height={700}
-        width={700}
-        rowCount={100}
-        rowHeight={50}
-        rowRenderer={({index,key,style,parent}) => {
-          const post = posts[index]
-          // console.log(post)
-          return (
-            <div key={key} style={style}>
-              <p>
-                {post.body}
-              </p>
-            </div>
-          )
-        }}
-      />
+                return (
+                  <CellMeasurer
+                    cache={cash.current}
+                    key={key}
+                    rowIndex={index}
+                    columnIndex={0}
+                    parent={parent}
+                  >
+                    <div style={style}>
+                      <PostCard body={post.body} title={post.title} />
+                    </div>
+                  </CellMeasurer>
+                );
+              }}
+            />
+          )}
+        </AutoSizer>
+      </div>
     </div>
   );
 };
---
+++ src/components/PostCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PostCard = ({body,title}) => {
+    return ( 
+        <div>
+                
+        </div>
+     );
+}
+
+// prop types
+PostCard.propTypes = {
+    body:PropTypes.string,
+    title:PropTypes.string
+}
+
+export default PostCard;
