9644d520a4eadf409ec18ac9e12fa73608824bcb
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-07-03 19:42:05 +0430

    feat: fetch posts
    fetch posts and show theme inside of list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-function App() {
+import React from "react";
+import { List, AutoSizer } from "react-virtualized";
+
+import useSWR from "swr";
+
+const App = () => {
+  const { data: posts } = useSWR(`https://jsonplaceholder.typicode.com/posts`);
+
   return (
-    <div className="App">
+    <div className="px-10 py-6 bg-slate-400 min-h-screen">
       <header className="App-header"></header>
       <h1 className="text-xl text-slate-500">saman</h1>
+
+      <List
+        height={700}
+        width={700}
+        rowCount={100}
+        rowHeight={50}
+        rowRenderer={({index,key,style,parent}) => {
+          const post = posts[index]
+          // console.log(post)
+          return (
+            <div key={key} style={style}>
+              <p>
+                {post.body}
+              </p>
+            </div>
+          )
+        }}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Loading from "./utils/Loading";
+// data fecthing
+import { SWRConfig } from "swr";
+import axios from "axios";
 
-
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Suspense fallback={<Loading />}>
+    <SWRConfig
+      value={{
+        refreshInterval: 5000,
+        suspense: true,
+        fetcher: (...args) => axios.get(args).then((res) => res.data),
+      }}
+    >
+      <App />
+    </SWRConfig>
+  </Suspense>
 );
-
